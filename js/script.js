10160e5813da8c639f57ef7fd961639e3d118912
Eduardo <74016606+eduardodiniz01@users.noreply.github.com>
2023-06-14 19:57:06 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,36 @@
+var banner = document.getElementById("shirt");
+var next = 0;
+
+
+//SLIDER
+function frente(){
+    next ++;
+    if(next < 7 ){
+    document.getElementById("shirt").src= `img/${next}.png`;
+    } else{
+        next = 0
+    }
+    }
+
+    function tras(){
+    next ++;
+    if(next < 7 ){
+    document.getElementById("shirt").src= `img/${next}.png`;
+    } else{
+        next = 0
+    }
+    }
+
+//MENU
+    
+
+function menu(){
+    document.getElementsByClassName("drop")[0].classList.toggle("drop_back");
+}
+
+function sumir(){
+    document.getElementsByClassName("drop")[0].classList.toggle("drop_back");
+}
+
+
+
